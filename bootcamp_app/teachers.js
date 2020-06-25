03d72f8cc1debc99f0534dc4750920c0ba8b0190
julimancan <julian.m.bustos@gmail.com>
2020-06-24 20:42:29 -0400

    added node queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+  user: 'juliman',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort 
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${process.argv[2]}'
+ORDER BY teacher;
+`)
+.then(res => {
+  // console.log(res.rows)
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
